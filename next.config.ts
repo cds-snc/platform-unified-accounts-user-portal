b77cfae9de2876feaafa8f4803cb53790851b015
Pat Heard <patrick.heard@cds-snc.ca>
2026-06-12 14:52:13 -0400

    fix: security header improvements, set in alpha order

--- next.config.ts
+++ next.config.ts
@@ -8,28 +8,33 @@ declare global {
 
 const securityHeaders = [
   {
-    key: "Strict-Transport-Security",
-    value: "max-age=63072000; includeSubDomains; preload",
+    key: "Cross-Origin-Opener-Policy",
+    value: "same-origin",
   },
   {
-    key: "X-Frame-Options",
-    value: "DENY",
+    key: "Cross-Origin-Resource-Policy",
+    value: "same-origin",
   },
   {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
+    key: "Permissions-Policy",
+    value:
+      "camera=(), microphone=(), geolocation=(), payment=(), browsing-topics=(), usb=(), serial=(), hid=(), bluetooth=()",
   },
   {
     key: "Referrer-Policy",
     value: "strict-origin-when-cross-origin",
   },
   {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=(), payment=(), browsing-topics=()",
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
   },
   {
-    key: "Cross-Origin-Opener-Policy",
-    value: "same-origin",
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
   },
 ];
 
