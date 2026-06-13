dbfb47ba324eb96d703af4012ced91b0c72f52a1
Pat Heard <patrick.heard@cds-snc.ca>
2026-06-13 11:10:28 -0400

    fix: improve security of headers and cookies (#273)
    Update the HTTP headers and cookie settings to improve the portals security posture:
    
    - `X-XSS-Protection` removed as it is deprecated and can cause vulnerabilities.
    - `Referrer-Policy` added so that only the domain is sent as the referrer header.
    - `Permissions-Policy` disables all listed browser features as they are not needed.
    - `Cross-Origin-Opener-Policy` blocks cross-origin sites from opening the portal in a popup.
    - `Cross-Origin-Resource-Policy` prevents assets from being embedded in an external site.
    
    The fingerprint cookie changes enforce sameSite and HTTPS access.
    
    Additionally this updates the proxy `matcher` to run on all requests as it is currently not 
    setting CSPs on the root path. This will cause the proxy to run on all static file requests.

--- proxy.ts
+++ proxy.ts
@@ -3,22 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { ZITADEL_ORGANIZATION } from "@root/constants/config";
 import { generateCSP, responseWithCSP } from "@lib/cspScripts";
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization)
-     * - favicon.ico (favicon file)
-     * - public folder files
-     * - files with extensions (images, fonts, etc.)
-     */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\..*|img/).*)",
-  ],
-};
-
 BigInt.prototype.toJSON = function () {
   return this.toString();
+};
+
+export const config = {
+  matcher: [
+    "/:path*", // Match all paths
+  ],
 };
 
 export async function proxy(request: NextRequest) {
