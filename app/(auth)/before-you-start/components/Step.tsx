33c137d2e40ecd675c88fcf45a3adc3dd0470b88
Tim Arney <Tim Arney>
2026-07-21 09:28:24 -0400

    cleanup

--- app/(auth)/before-you-start/components/Step.tsx
+++ app/(auth)/before-you-start/components/Step.tsx
@@ -1,9 +1,54 @@
-export const Step = ({ key, icon }: { key: string; icon: React.ReactNode }) => {
+/*--------------------------------------------*
+ * Framework and Third-Party
+ *--------------------------------------------*/
+import React from "react";
+
+import { cn } from "@lib/utils";
+/*--------------------------------------------*
+ * Internal Aliases
+ *--------------------------------------------*/
+import { getImageUrl } from "@lib/utils/imageUrl";
+import { I18n } from "@i18n";
+import { Image } from "@components/ui/image/Image";
+
+export const Step = ({
+  titleKey,
+  descKey,
+  iconSrc,
+  namespace = "beforeYouStart",
+  children,
+  className,
+}: {
+  titleKey: string;
+  descKey?: string;
+  iconSrc: string;
+  namespace?: string;
+  children?: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <div>
-      <div>{icon}</div>
-      <div>Title</div>
-      <div>Desc</div>
+    <div className={cn("flex items-start gap-5", className)}>
+      <div className="flex h-12 w-10 items-center justify-center rounded-xl border-1 border-gcds-purple-450 bg-violet-50">
+        <Image src={getImageUrl(iconSrc)} alt="" width={24} height={24} className="flex-none" />
+      </div>
+      <div className="min-w-0">
+        <I18n
+          i18nKey={titleKey}
+          namespace={namespace}
+          tagName="div"
+          className="text-1xl mb-1 font-bold"
+        />
+        {descKey && (
+          <I18n
+            i18nKey={descKey}
+            namespace={namespace}
+            tagName="div"
+            className="text-1xl text-gcds-gray-800"
+          />
+        )}
+
+        {children}
+      </div>
     </div>
   );
 };
--- app/(auth)/before-you-start/page.tsx
+++ app/(auth)/before-you-start/page.tsx
@@ -10,18 +10,49 @@ import { SearchParams } from "@lib/utils";
 import { serverTranslation } from "@i18n/server";
 import { AuthPanel } from "@components/auth/AuthPanel";
 
+import { CallOut } from "./components/CallOut";
+import { Step } from "./components/Step";
 /*--------------------------------------------*
  * Parent Relative
  *--------------------------------------------*/
 import { Title } from "./components/Title";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const { t } = await serverTranslation("beforeYouStart");
+  return { title: t("title") };
+}
 
 export default async function Page(props: { searchParams: Promise<SearchParams> }) {
   const searchParams = await props.searchParams;
   const { requestId } = searchParams;
 
   return (
-    <AuthPanel titleI18nKey="none" descriptionI18nKey="" namespace="beforeYouStart">
-      <Title />
-    </AuthPanel>
+    <div data-request-id={requestId}>
+      <AuthPanel titleI18nKey="none" descriptionI18nKey="" namespace="beforeYouStart">
+        <Title />
+        <div className="mt-6 space-y-6">
+          {/* Step 1: Verify your email address */}
+
+          <Step
+            titleKey="step1.title"
+            descKey="step1.description"
+            iconSrc="/img/email_@.svg"
+          ></Step>
+
+          <CallOut i18nKey="step1.callout" className="mb-10" />
+
+          {/* Step 2: Set up two-factor authentication */}
+          <Step
+            titleKey="step2.title"
+            descKey="step2.description"
+            iconSrc="/img/lock__circle_open.svg"
+          ></Step>
+          <div className="mt-3">
+            <CallOut iconSrc="/img/fingerprint_sm_blk.svg" i18nKey="step2.callout1" />
+            <CallOut iconSrc="/img/qr_code_scanner.svg" i18nKey="step2.callout2" />
+          </div>
+        </div>
+      </AuthPanel>
+    </div>
   );
 }
