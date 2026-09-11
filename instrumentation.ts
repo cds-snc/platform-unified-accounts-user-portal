2ccbeb36ba1ddee0ba2817091a513c7fc1bcda28
Pat Heard <patrick.heard@cds-snc.ca>
2026-09-11 10:44:54 -0400

    fix: control metrics with env var

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,5 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
+  if (process.env.RUNTIME_METRICS === "1") {
     const { registerRuntimeMetrics } = await import("./instrumentation.node");
     registerRuntimeMetrics();
   }
