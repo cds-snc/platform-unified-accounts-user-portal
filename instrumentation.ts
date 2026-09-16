2eb3c8a0091b3084d3934d1c43cce207cc108735
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2026-09-16 14:14:07 -0400

    fix edge runtime error

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,5 @@
 export async function register() {
-  if (process.env.RUNTIME_METRICS === "1") {
+  if (process.env.RUNTIME_METRICS === "1" && process.env.NEXT_RUNTIME !== "edge") {
     const { registerRuntimeMetrics } = await import("./instrumentation.node");
     registerRuntimeMetrics();
   }
